381d5ba55f08c12042a1f83a8d0db40aa2915f59
saneku <sasha.ukhov@gmail.com>
2025-02-03 17:37:23 +0300

    minor

--- core/netcdf_handler.py
+++ core/netcdf_handler.py
@@ -1,60 +1,64 @@
 import netCDF4 as nc
+import numpy as np
+import os
+import xarray as xr
+import datetime
+import pandas as pd
 
 class NetCDFHandler:
-    def __init__(self, source_dir)#, destination_file):
+    def __init__(self, source_dir):#, destination_file):
         self.source_dir = source_dir
         #self.destination_file = destination_file
-
 
         self.orgn_wrf_input_file="wrfinput_d01"
         self.dst_file="wrfchemv_d01"
 
-
         #===========================================
         #Read wrfinput data
-        print (f'Open {self.source_dir}{orgn_wrf_input_file}')
-        wrfinput=Dataset(f'{self.source_dir}{orgn_wrf_input_file}','r')
-        self.xlon=wrfinput.variables['XLONG'][0,:]
-        self.xlat=wrfinput.variables['XLAT'][0,:]
-        MAPFAC_MX=wrfinput.variables['MAPFAC_MX'][0,:]
-        MAPFAC_MY=wrfinput.variables['MAPFAC_MY'][0,:]
+        print (f'Open {self.source_dir}{self.orgn_wrf_input_file}')
+        with nc.Dataset(f'{self.source_dir}{self.orgn_wrf_input_file}','r') as wrfinput:
+            self.xlon=wrfinput.variables['XLONG'][0,:]
+            self.xlat=wrfinput.variables['XLAT'][0,:]
+            MAPFAC_MX=wrfinput.variables['MAPFAC_MX'][0,:]
+            MAPFAC_MY=wrfinput.variables['MAPFAC_MY'][0,:]
 
-        dy = wrfinput.getncattr('DY')
-        dx = wrfinput.getncattr('DX')
-        self.surface = (dx/MAPFAC_MX)*(dy/MAPFAC_MY)       #surface in m2
+            dy = wrfinput.getncattr('DY')
+            dx = wrfinput.getncattr('DX')
+            self.surface = (dx/MAPFAC_MX)*(dy/MAPFAC_MY)       #surface in m2
 
-        self.Z = (wrfinput.variables['PH'][0,:] + wrfinput.variables['PHB'][0,:]) / 9.81
-        dz = np.diff(self.Z,axis=0)
+            self.Z = (wrfinput.variables['PH'][0,:] + wrfinput.variables['PHB'][0,:]) / 9.81
+            dz = np.diff(self.Z,axis=0)
 
-        self.Z = self.Z[:-1]
-        self.Z = self.Z + dz*0.5
-        self.Z = self.Z/1000.0
+            self.Z = self.Z[:-1]
+            self.Z = self.Z + dz*0.5
+            self.Z = self.Z/1000.0
 
-        wrfinput.close()
-        
         #===========================================
         #copy wrfinput to wrfchemv
-        if os.path.exists(f'{self.source_dir}{dst_file}'):
-            os.system(f'rm {self.source_dir}{dst_file}')
+        if os.path.exists(f'{self.source_dir}{self.dst_file}'):
+            os.system(f'rm {self.source_dir}{self.dst_file}')
 
-        ds = xr.open_dataset(f'{self.source_dir}{orgn_wrf_input_file}')
+        ds = xr.open_dataset(f'{self.source_dir}{self.orgn_wrf_input_file}')
         ds_var = ds[['PH','PHB','T','Times']]
-        ds_var.to_netcdf(f'{self.source_dir}{dst_file}')
+        ds_var.to_netcdf(f'{self.source_dir}{self.dst_file}')
         #===========================================
         
-        #here
-        wrf_volc_file = Dataset(f'{orgn_dir}{dst_file}','r+')
-        add2dVar(wrf_volc_file,"ERUP_BEG","START TIME OF ERUPTION","?")
-        add2dVar(wrf_volc_file,"ERUP_END","END TIME OF ERUPTION","?")
-        add3dVar(wrf_volc_file,"E_VSO2","Volcanic Emissions, SO2","mol/m2/h")
-        add3dVar(wrf_volc_file,"E_VSULF","Volcanic Emissions, SULF","mol/m2/h")
-        add3dVar(wrf_volc_file,"E_QV","Volcanic Emissions, QV","kg/m2/s")
+        #add variables to wrfchemv
+        with nc.Dataset(f'{self.source_dir}{self.dst_file}','r+') as wrf_volc_file:
+            self.__add2dVar(wrf_volc_file,"ERUP_BEG","START TIME OF ERUPTION","?")
+            self.__add2dVar(wrf_volc_file,"ERUP_END","END TIME OF ERUPTION","?")
+            self.__add3dVar(wrf_volc_file,"E_VSO2","Volcanic Emissions, SO2","mol/m2/h")
+            self.__add3dVar(wrf_volc_file,"E_VSULF","Volcanic Emissions, SULF","mol/m2/h")
+            self.__add3dVar(wrf_volc_file,"E_QV","Volcanic Emissions, QV","kg/m2/s")
 
-        for i in range(1,11):
-            add3dVar(wrf_volc_file,"E_VASH"+str(i),"Volcanic Emissions, bin"+str(i),"ug/m2/s")
+            for i in range(1,11):
+                self.__add3dVar(wrf_volc_file,"E_VASH"+str(i),"Volcanic Emissions, bin"+str(i),"ug/m2/s")
 
 
+    def __str__(self):
+        return f"NetCDFHandler(source_file='{self.source_dir}{self.orgn_wrf_input_file}', destination_file='{self.source_dir}{self.dst_file}', dimensions={self.Z.shape})"
 
+    '''
     def write_data(self, emissions):
         with nc.Dataset(self.destination_file, 'w', format='NETCDF4') as dataset:
             dataset.createDimension('emission', len(emissions))
@@ -68,22 +72,59 @@ class NetCDFHandler:
             amounts[:] = [e.amount for e in emissions]
             durations[:] = [e.duration for e in emissions]
             start_times[:] = [e.start_time for e in emissions]
+    '''
+    
+    def prepare_file(self,start_time,end_time,interval_days,interval_hours,interval_mins):
+        with nc.Dataset(f'{self.source_dir}{self.dst_file}', 'a') as dataset:
+            #days=range(0,366)
+            time_range = pd.date_range(start=start_time, end=end_time, 
+                           freq=pd.Timedelta(days=interval_days, hours=interval_hours, minutes=interval_mins))
 
-    def prepare_file():
-        dataset = nc.Dataset(self.destination_file, 'a')
-        days=range(0,366)
-        aux_times = np.chararray((len(days), 19), itemsize=1)
-        start_date = datetime(2016, 1, 1, 0, 0)
-        for i, day in enumerate(days):
-            aux_date = start_date + timedelta(days=day)
-            aux_times[i] = list(aux_date.strftime("%Y-%m-%d_%H:%M:%S"))
-        dataset.variables['Times'][:] = aux_times
-        # Loop through all variables and copy the first time step to the new time steps
-        for var_name in dataset.variables:
-            if var_name == 'Times':
-                continue
-            var = dataset.variables[var_name]
-            if 'Time' in var.dimensions:
-                print(var)
-                for i in days:
-                    var[i,:]=var[0,:]
+            # Convert to array if needed
+            #time_array = np.array(time_range)
+
+            
+            aux_times = np.chararray((len(time_range), 19), itemsize=1)
+            
+            for i, aux_date in enumerate(time_range):
+                #aux_date = start_time + datetime.timedelta(days=interval_days,hours=interval_hours,minutes=interval_mins)
+                aux_times[i] = list(aux_date.strftime("%Y-%m-%d_%H:%M:%S"))
+            dataset.variables['Times'][:] = aux_times
+            
+            # Loop through all variables and create a new times
+            for var_name in dataset.variables:
+                if var_name == 'Times':
+                    continue
+                var = dataset.variables[var_name]
+                if 'Time' in var.dimensions:
+                    print(var)
+                    for i,_ in enumerate(aux_times):
+                        var[i,:]=var[0,:]
+    
+    
+    def __add3dVar(self,wrf_file,var_name,caption,units):
+        wrf_file.createVariable(var_name, 'f4', ('Time','bottom_top','south_north', 'west_east'))
+        wrf_file.variables[var_name].FieldType=104
+        wrf_file.variables[var_name].MemoryOrder="XYZ"
+        wrf_file.variables[var_name].description=caption
+        wrf_file.variables[var_name].units=units
+        wrf_file.variables[var_name].stagger = "" ;
+        wrf_file.variables[var_name].coordinates = "XLONG XLAT XTIME"
+
+        #zero field
+        wrf_file.variables[var_name][:] = 0.0
+        print (f"Adding {var_name} {caption} {units} into {wrf_file}")
+
+
+    def __add2dVar(self, wrf_file, var_name, caption, units):
+        wrf_file.createVariable(var_name, 'f4', ('Time','south_north', 'west_east'))
+        wrf_file.variables[var_name].FieldType=104
+        wrf_file.variables[var_name].MemoryOrder="XY"
+        wrf_file.variables[var_name].description=caption
+        wrf_file.variables[var_name].units=units
+        wrf_file.variables[var_name].stagger = "" ;
+        wrf_file.variables[var_name].coordinates = "XLONG XLAT"
+
+        #zero field
+        wrf_file.variables[var_name][:] = 0.0
+        print (f"Adding {var_name} {caption} {units} into {wrf_file}")
--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from profiles.types import UniformProfile, SuzukiProfile, InvertedProfile
 from core.eruption import Eruption
 from core.netcdf_handler import NetCDFHandler
 from core.writer import EmissionWriter
+import datetime
 
 if __name__ == "__main__":
+    '''
     emissions = [
         AshEmission(mean=50, stddev=10, location="A", amount=100, duration=5, start_time="12:00"),
         SO2Emission(mean=10, stddev=0.5, location="B", amount=200, duration=10, start_time="14:00"),
@@ -19,11 +21,19 @@ if __name__ == "__main__":
     volc_so2_emis_0=[15.0] #Mt
     volc_sulf_emis_0=[0.0] #Mt
     volc_qv_emis_0=[150.0] #Mt
+    '''
+
+    start_time = datetime.datetime(2016, 1, 1, 0, 0)
+    end_time = datetime.datetime(2016, 1, 1, 0, 40)
 
 
-
-
-    netcdf_handler = NetCDFHandler(source_file="source.nc", destination_file="output.nc")
+    netcdf_handler = NetCDFHandler(source_dir="./")
+    print(netcdf_handler)
+    #def prepare_file(self,start_time,interval_days,interval_hours,interval_mins):
+    netcdf_handler.prepare_file(start_time,end_time,interval_days=0,interval_hours=0,interval_mins=10)
+    exit()
+    
+    
     emission_writer = EmissionWriter(emissions, netcdf_handler)
 
     eruption = Eruption()
