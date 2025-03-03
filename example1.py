a02b694b785791a67abcf3ac2a48266dd3e1c408
Alexander Ukhov <saneku@users.noreply.github.com>
2025-03-03 23:30:22 +0300

    updated case with emissions of everything

--- example1.py
+++ example1.py
@@ -1,35 +1,84 @@
 from core import *
 from emissions import *
 from profiles import *
+import numpy as np
 
-# Example 1: Different type profiles with the same duration DURATION = 7200 seconds
+# Example 1: Different type profiles with the same DURATION = 7200 seconds
 
 if __name__ == "__main__":
     LAT, LON = 15.1429, 120.3496
     YEAR, MONTH, DAY = 1991, 6, 15
     DURATION = 7200
 
-    ash_scenario = EmissionScenario_MixOfProfiles(
-        Emission_Ash(mass_mt=66.53, lat=LAT, lon=LON, bin_n=10, mean_r=2.4, stddev=1.8)
-    )
+    #Ash
+    e=Emission_Ash(mass_mt=65.0, lat=LAT, lon=LON)
+    e.setMassFractions(np.full(10, 0.1))
+    ash_scenario = EmissionScenario_MixOfProfiles(e)
 
     netcdf_handler = WRFNetCDFWriter(source_dir="./")
     j, i = netcdf_handler.findClosestGridCell(lat=LAT, lon=LON)
     staggerred_h = netcdf_handler.getColumn_H(i, j)
 
-    profiles = [
-        (VerticalProfile_Umbrella, [staggerred_h, YEAR, MONTH, DAY, 3, DURATION, 15000, 1000, 0.55]),
-        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 5, DURATION]),
-        (VerticalProfile_Umbrella, [staggerred_h, YEAR, MONTH, DAY, 7, DURATION, 25000, 1000, 0.95]),
-        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 9, DURATION]),
-        (VerticalProfile_Uniform, [staggerred_h, YEAR, MONTH, DAY, 11, DURATION, 1.0, 5000.0, 10000.0]),
-        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 13, DURATION])
+    ash_profiles = [
+        (VerticalProfile_Umbrella, [staggerred_h, YEAR, MONTH, DAY, 2, DURATION, 15000, 1000, 0.55]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 4, DURATION]),
+        (VerticalProfile_Umbrella, [staggerred_h, YEAR, MONTH, DAY, 6, DURATION, 25000, 1000, 0.95]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 8, DURATION]),
+        (VerticalProfile_Uniform, [staggerred_h, YEAR, MONTH, DAY, 10, DURATION, 1.0, 5000.0, 10000.0]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 12, DURATION])
     ]
-
-    for p, args in profiles:
+    for p, args in ash_profiles:
         ash_scenario.add_profile(p(*args))
 
-    emission_writer = EmissionWriter_UniformInTimeProfiles([ash_scenario], netcdf_handler, DURATION/60)
+    #SO2 Emissions
+    so2_scenario = EmissionScenario_MixOfProfiles(Emission_SO2(mass_mt=15.0,lat=LAT, lon=LON))
+    so2_profiles = [
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 2, DURATION]),
+        (VerticalProfile_Umbrella, [staggerred_h, YEAR, MONTH, DAY, 4, DURATION, 25000, 1000, 0.95]),
+        (VerticalProfile_Umbrella, [staggerred_h, YEAR, MONTH, DAY, 6, DURATION, 15000, 1000, 0.75]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 8, DURATION]),
+        (VerticalProfile_Umbrella, [staggerred_h, YEAR, MONTH, DAY, 10, DURATION, 10000, 1000, 0.55]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 12, DURATION])
+    ]
+    
+    for p, args in so2_profiles:
+        so2_scenario.add_profile(p(*args))
+
+    #Sulfate Emissions
+    sulfate_scenario = EmissionScenario_MixOfProfiles(Emission_Sulfate(mass_mt=5.0,lat=LAT, lon=LON))
+    sulfate_profiles = [
+        (VerticalProfile_Umbrella, [staggerred_h, YEAR, MONTH, DAY, 2, DURATION, 10000, 1000, 0.95]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 4, DURATION]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 6, DURATION]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 8, DURATION]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 10, DURATION]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 12, DURATION])
+    ]
+    for p, args in sulfate_profiles:
+        sulfate_scenario.add_profile(p(*args))
+
+    #Water vapor Emissions
+    watervapor_scenario = EmissionScenario_MixOfProfiles(Emission_WaterVapor(mass_mt=25.0,lat=LAT, lon=LON))
+    watervapor_profiles = [
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 2, DURATION]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 4, DURATION]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 6, DURATION]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 8, DURATION]),        
+        (VerticalProfile_Umbrella, [staggerred_h, YEAR, MONTH, DAY, 10, DURATION, 10000, 1000, 0.95]),
+        (VerticalProfile_Zero, [staggerred_h, YEAR, MONTH, DAY, 12, DURATION]),
+    ]
+    for p, args in watervapor_profiles:
+        watervapor_scenario.add_profile(p(*args))
+
+    #Adding all scenarios
+    emission_writer = EmissionWriter_UniformInTimeProfiles([ash_scenario,so2_scenario,sulfate_scenario,watervapor_scenario], \
+                                                           netcdf_handler, DURATION/60)
     emission_writer.write()
+
+
+    ash_scenario.plot()
+    so2_scenario.plot()
+    sulfate_scenario.plot()
+    watervapor_scenario.plot()    
 
     netcdf_handler.plot_how_much_was_written()
