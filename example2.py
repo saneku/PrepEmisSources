ff0e45c45a1ebbc3df1c9cce05d511d3f6e2ec77
saneku <sasha.ukhov@gmail.com>
2025-05-08 16:57:35 +0300

    minor

--- example2.py
+++ example2.py
@@ -3,7 +3,8 @@ from src import *
 import numpy as np
 
 #example 2
-#Non uniform in time emisson profiles of ash and SO2 are taken from the Ukhov et al. (2023).
+#emisson profiles of ash and SO2 from Ukhov et al. (2023).
+#water vapor emission is set to 100 Mt, following the SO2 emission scenario
 
 if __name__ == "__main__":
     LAT, LON = 15.1429, 120.3496
@@ -13,15 +14,18 @@ if __name__ == "__main__":
                                                     './example_profiles/Pinatubo_Ukhov_2023/ash_2d_emission_profiles'),
 
                 EmissionScenario_InvertedPinatubo(Emission_SO2(mass_mt=15.54,lat=LAT, lon=LON),
+                                                     './example_profiles/Pinatubo_Ukhov_2023/so2_2d_emission_profiles'),
+                
+                EmissionScenario_InvertedPinatubo(Emission_WaterVapor(mass_mt=100.0,lat=LAT, lon=LON),
                                                      './example_profiles/Pinatubo_Ukhov_2023/so2_2d_emission_profiles')
                 #Emission_Sulfate(mass_mt=0.1,lat=15,lon=165),
-                #Emission_WaterVapor(mass_mt=150,lat=15,lon=165)
                 ]
     #scenarios[0].plot(linestyle='--', color='grey', marker='')
     netcdf_handler = WRFNetCDFWriter(source_dir="./")
 
     scenarios[0].plot()
     scenarios[1].plot()
+    scenarios[2].plot()
 
     emission_writer = EmissionWriter_NonUniformInTimeProfiles(scenarios, netcdf_handler, 10)
     emission_writer.write()
@@ -33,7 +37,7 @@ if __name__ == "__main__":
 
     scenarios[0].plot()
     scenarios[1].plot()
-
+    scenarios[2].plot()
     
     netcdf_handler.plot_how_much_was_written()
 
